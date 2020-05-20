c37573c65c2c3882e1420fe8a186673c0d6ffee9
alexander-shvets <quadratoff@gmail.com>
2020-05-20 04:10:46 +0300

    first commit

---
+++ precache-manifest.67cdb80cf2e85ffaae8a365a6a81d804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6841ac7b3beef53e533e",
+    "url": "/css/chunk-vendors.css"
+  },
+  {
+    "revision": "c26ff823fbf4f9b447a1f3aac46df2d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cfee6f44fa9d1ec3c44",
+    "url": "/js/app.js"
+  },
+  {
+    "revision": "6841ac7b3beef53e533e",
+    "url": "/js/chunk-vendors.js"
+  },
+  {
+    "revision": "5554c54ce6f1c5a0739c50e60e56305b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
